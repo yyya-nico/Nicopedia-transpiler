45c3c49027bbe9f6ffda730c85d19d32f954e6f5
yyya_nico <30824964+yyya-nico@users.noreply.github.com>
2025-03-17 21:14:50 +0900

    Astro v5の対応

---
+++ src/content.config.ts
@@ -0,0 +1,12 @@
+import { z, defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+const posts = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.mdx?', base: './src/content/posts' }),
+  schema: z.object({
+    urlSnippet: z.string(),
+    title: z.string()
+  }),
+});
+
+export const collections = { posts };
