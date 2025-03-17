17a4f800ffe0e0f6f21e7c04568d52aa5d978c02
yyya_nico <30824964+yyya-nico@users.noreply.github.com>
2025-03-17 21:47:59 +0900

    書き間違いの修正

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { z, defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const posts = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.mdx?', base: './src/content/posts' }),
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './src/content/posts' }),
   schema: z.object({
     urlSnippet: z.string(),
     title: z.string()
